512834642293b2d62912a18bac68a29f521a43e0
dmathieu <42@dmathieu.com>
2011-01-05 18:37:40 +0100

    name the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 Rails.application.routes.draw do
   scope '/oauth' do
-    get '/' => 'oauned/oauth#index'
+    get '/' => 'oauned/oauth#index', :as => 'oauth'
     post '/' => 'oauned/oauth#authorize'
     post '/token' => 'oauned/oauth#token'
   end
