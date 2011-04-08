61dfc4b7c41ca4029f8479438145d709a235e8be
dmathieu <42@dmathieu.com>
2011-04-08 17:57:54 +0200

    this is not haml anymore. Render the application's name properly

--- spec/controllers/oauth_controller_index_spec.rb
+++ spec/controllers/oauth_controller_index_spec.rb
@@ -2,7 +2,9 @@ require 'spec_helper'
 require 'cgi'
 
 describe Oauned::OauthController do
-  let(:application) { Application.create!(:redirect_uri => 'http://www.example.net') }
+  render_views
+  
+  let(:application) { Application.create!(:redirect_uri => 'http://www.example.net', :name => "My Application") }
   let(:user) { User.create!}
   
   before :each do
@@ -28,6 +30,11 @@ describe Oauned::OauthController do
     it 'should render the index page' do
       get :index, :client_id => application.id, :redirect_uri => application.redirect_uri
       response.should be_success
+    end
+    
+    it "should display the application's name" do
+      get :index, :client_id => application.id, :redirect_uri => application.redirect_uri
+      response.body.match(Regexp.new(application.name)).should_not be_nil
     end
     
     it "should redirect if the user is not logged in" do
