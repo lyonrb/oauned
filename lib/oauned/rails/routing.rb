08a8c58065f2ecd5f1b60b559f785e557fc17276
Damien Mathieu <42@dmathieu.com>
2011-09-07 10:14:09 +0200

    remove empty lines

--- lib/oauned/rails/routing.rb
+++ lib/oauned/rails/routing.rb
@@ -1,6 +1,6 @@
 module ActionDispatch::Routing
   class Mapper
-    
+
     def oauned_routing
       get '/' => 'oauned/oauth#index', :as => 'oauth'
       post '/' => 'oauned/oauth#authorize'
--- spec/controllers_spec.rb
+++ spec/controllers_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "controllers" do
-  
+
   [:oauth_user, :normal_user, :current_user].each do |method|
     it "should have the #{method} defined" do
       ApplicationController.new.respond_to?(method).should be_true
