95be98672c70d076b8ae382f1b4abfecf3f341b4
Damien Mathieu <42@dmathieu.com>
2012-01-10 11:52:04 +0100

    use the dynamic class reference in the controller methods

--- lib/oauned/controller_methods.rb
+++ lib/oauned/controller_methods.rb
@@ -3,11 +3,11 @@ module Oauned
     def self.included(klass)
       klass.class_eval do
         cattr_accessor :oauth_options, :oauth_options_proc
-          
+
         protected
         def self.deny_oauth(options = {}, &block)
           raise 'options cannot contain both :only and :except' if options[:only] && options[:except]
-        
+
           [:only, :except].each do |k|
             if values = options[k]
               options[k] = Array(values).map(&:to_s).to_set
@@ -16,22 +16,22 @@ module Oauned
           self.oauth_options = options
           self.oauth_options_proc = block
         end
-          
+
         def oauth_user
           @oauth_user ||= oauth_allowed? ? user_from_oauth : nil
-        end          
-          
+        end
+
         alias :normal_user :current_user
         def current_user
           normal_user || oauth_user
         end
-        
+
         private
         def user_from_oauth
-          token = Connection.where(['access_token LIKE ?', params[:access_token]]).first
+          token = Oauned::Models['connection'].where(['access_token LIKE ?', params[:access_token]]).first
           token.user if (token && !token.expired?)
         end
-        
+
         def oauth_allowed?
           return true if (oauth_options_proc && !oauth_options_proc.call(self)) || oauth_options.nil?
           return false if oauth_options.empty?
